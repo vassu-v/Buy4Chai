ccbf2a4d148e1a9bf0ee6eaa5b68ca850a72caee
vassu-v <shoryavardhaans2@gmail.com>
2026-06-24 18:08:17 +0530

    feat: add Vercel Analytics and Google site verification

--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Landing from './landing.jsx'
 import Playground from './Playground.jsx'
 import './index.css'
@@ -13,5 +14,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/playground" element={<Playground />} />
       </Routes>
     </BrowserRouter>
+    <Analytics />
   </React.StrictMode>,
 )
